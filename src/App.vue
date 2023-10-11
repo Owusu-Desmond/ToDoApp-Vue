c3c008f2bcdcb7982b1abe33dd73a59a439264cf
Owusu-Desmond <desmond0406114009@gmail.com>
2023-10-11 14:27:36 +0000

    Setup project with npm create vue app

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+  count.value++
+}
+
+function decrement() {
+  count.value--
+}
+
+onMounted(() => {
+  console.log('Component is mounted!')
+})
+</script>
+<template>
+  <h1>Counter</h1>
+
+  <button @click="decrement">-</button>
+  <span :style="{color: 'red'}">{{ count }}</span>
+  <button @click="increment">+</button>
+
+
+</template>
